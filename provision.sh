c61f68625a547095556fa367bca5daf6e7559818
Jake Bladt <jake@ambulnz.com>
2017-02-17 17:19:55 -0500

    configure go

--- provision.sh
+++ provision.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
+
+# install tools
 apt-get update
 apt-get install -y python-software-properties
 add-apt-repository -y ppa:git-core/ppa
 apt-get update
 apt-get install -y git
+apt-get install -y nfs-common portmap
 
-
+# configure golang
+su vagrant -c "echo 'GOROOT=/usr/local/go' >> ~/.profile"
+su vagrant -c "echo 'GOPATH=/home/vagrant/gocode' >> ~/.profile"
+su vagrant -c "echo 'GOBIN=\$GOPATH/bin' >> ~/.profile"
+su vagrant -c "echo 'PATH=\$PATH:\$GOBIN' >> ~/.profile"
+su vagrant -c "mkdir -p /home/vagrant/gocode"
