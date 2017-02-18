e4520a13608b58418ee771de1a2e351b66d90b69
Jake Bladt <jake@jakebladt.com>
2017-02-18 12:21:43 -0500

    configure for root/vagrant

--- provision.sh
+++ provision.sh
@@ -11,19 +11,13 @@ apt-get install -y nfs-common portmap
 # install go
 wget https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.8.linux-amd64.tar.gz
-mkdir /gopath
 
-# configure golang for vagrant user
-su vagrant -c "echo 'GOROOT=/usr/local/go' >> ~/.profile"
-su vagrant -c "echo 'GOPATH=/gopath' >> ~/.profile"
-su vagrant -c "echo 'GOBIN=\$GOPATH/bin' >> ~/.profile"
+# configure golang for all users
+mkdir -p /gopath
+echo GOROOT=/usr/local/go >> /etc/environment
+echo GOPATH=/gopath >> /etc/environment
+echo GOBIN=/gopath/bin >> /etc/environment
+
+# add golang to path for vagrant and root
 su vagrant -c "echo 'PATH=\$PATH:\$GOBIN' >> ~/.profile"
-su vagrant -c "mkdir -p /home/vagrant/gocode"
-
-# configure golang for new shells (like root)
-echo GOROOT=/usr/local/go >> ~/.profile
-echo GOPATH=/gopath >> ~/.profile
-su vagrant -c "echo 'GOBIN=\$GOPATH/bin' >> ~/.profile"
-su vagrant -c "echo 'PATH=\$PATH:\$GOBIN' >> ~/.profile"
-su vagrant -c "mkdir -p /home/vagrant/gocode"
-
+echo PATH=$PATH:$GOBIN >> ~/.profile
