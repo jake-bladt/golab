e9a5530bf46a21c32cb54b3d5e26f887ca8f2b6f
Jake Bladt <jake@jakebladt.com>
2017-02-18 20:07:51 -0500

    try /usr/local

--- provision.sh
+++ provision.sh
@@ -15,8 +15,8 @@ tar -C /usr/local -xzf go1.8.linux-amd64.tar.gz
 # configure golang for all users
 mkdir -p /gopath
 echo GOROOT=/usr/local/go >> /etc/environment
-echo GOPATH=/gopath >> /etc/environment
-echo GOBIN=/gopath/bin >> /etc/environment
+echo GOPATH=/usr/local/gopath >> /etc/environment
+echo GOBIN=/usr/local/gopath/bin >> /etc/environment
 
 # add golang to path for vagrant and root
 su vagrant -c "echo 'PATH=\$PATH:\$GOBIN:\$GOROOT/bin' >> ~/.profile"
